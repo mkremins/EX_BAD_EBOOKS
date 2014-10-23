e32704b56c0a7e313151796c685381ee0c89cf77
Max Kreminski <maxkreminski@gmail.com>
2014-10-22 23:25:30 -0700

    hackity hack

--- src/cryptic/emit.clj
+++ src/cryptic/emit.clj
@@ -7,38 +7,67 @@
     [:choose "Error:" "Warning:" "Exception:"]
     :nil])
 
+(def badjectives
+  ["bad" "invalid" "illegal" "improper" "missing" "incorrect" "insufficient"
+   "ambiguous" "malformed"])
+
+(defn badjective' []
+  (rand-nth (conj badjectives "no such")))
+
 (defn badjective []
-  [:choose
-    "no such" "bad" "invalid" "illegal" "improper" "missing" "incorrect"
-    "insufficient" "ambiguous" "malformed"])
+  (rand-nth badjectives))
+
+(def weirdjectives
+  ["ambitious" "operatic" "cryptic" "arcane" "archaic" "deliberate" "malicious"
+   "aggressive" "ambient" "mysterious" "abundant" "incessant" "weird" "strange"
+   "falsifiable" "impetuous" "skeptical" "forgettable" "laborious"
+   "performative" "impeccable" "hasty" "pneumatic" "arable" "incandescent"
+   "illegible" "aromatic" "flammable" "unfriendly" "omniscient" "benevolent"
+   "suboptimal" "unrecoverable" "moribund" "gelatinous" "ecstatic"
+   "indivisible" "implausible" "vacillating" "abhorrent" "abstract"
+   "productive" "intangible" "factual" "irrelevant" "monochromatic"
+   "undecidable" "entertaining" "fatuous" "gratuitous" "ingenuous"
+   "superfluous" "ironic" "ambivalent" "philosophical" "majestic" "grandiose"
+   "inarticulate" "immutable" "indirect" "monadic" "variadic" "inevitable"
+   "colossal" "mellifluous" "recondite" "abstruse" "recalcitrant" "obstinate"
+   "perverse" "defiant" "difficult" "wayward" "misleading" "inferior"
+   "laborious" "stereotypical" "obscure" "inscrutable" "excruciating"
+   "phosphorescent" "unfinished" "unbalanced" "impartial" "unqualified"
+   "derivative" "unoriginal" "particulate"])
+
+(defn weirdjective []
+  (rand-nth weirdjectives))
+
+(def nouns
+  ["accusation" "admixture" "assumption" "argument" "axiom" "batch"
+   "constraint" "corpus" "gender" "fugitive" "party" "entity" "word" "rule"
+   "number" "parameter" "operation" "suggestion" "advice" "insertion"
+   "deletion" "removal" "access" "symbol" "variable" "formula" "request"
+   "command" "action" "permission" "type" "name" "reference" "object" "plea"
+   "article" "inquiry" "query" "quandary" "question" "file" "path" "error"
+   "exception" "warning" "violation" "activity" "task" "performance" "branch"
+   "case" "switch" "function" "default" "handler" "clause" "handle" "queue"
+   "collection" "iterator" "item" "data" "transaction" "transformation"
+   "meme" "deity" "invocation" "matrix" "array" "set" "map" "hash" "test"
+   "condition" "consequent" "alternative" "scene" "graph" "plot" "wizard"
+   "anomaly" "prophecy" "operator" "functor" "application" "thread" "process"
+   "overflow" "jail" "escape" "recursion" "depth" "vector" "window"
+   "component" "operand" "machinery" "pipe" "widget" "spell" "arrow"
+   "database" "package" "statement" "expression" "justification" "parse"
+   "tree" "parser" "analyzer" "loader" "linker" "library" "module" "routine"
+   "class" "hierarchy" "attribute" "method" "callback" "monad" "architecture"
+   "target" "assembler" "language" "line" "column" "row" "scope" "closure"
+   "binding" "abstraction" "entry" "token" "trace" "hack" "dependency"
+   "resolution" "keyword" "stopword" "atom" "procedure" "directory" "mode"
+   "user" "flag" "box" "property" "phrase" "traceback" "stack" "heap"
+   "state" "arity" "leaf" "signal" "password" "character" "factory" "filter"
+   "lambda" "recipe" "transducer" "system" "interface" "implementation"
+   "logic" "form" "syntax" "icon" "effigy" "comment" "document" "namespace"
+   "primitive" "lexer" "reader" "loop" "event" "predicate" "promise" "job"
+   "rune" "monolith" "judge" "spline"])
 
 (defn noun []
-  [:choose
-    "accusation" "admixture" "assumption" "argument" "axiom" "batch"
-    "constraint" "corpus" "gender" "fugitive" "party" "entity" "word" "rule"
-    "number" "parameter" "operation" "suggestion" "advice" "insertion"
-    "deletion" "removal" "access" "symbol" "variable" "formula" "request"
-    "command" "action" "permission" "type" "name" "reference" "object" "plea"
-    "article" "inquiry" "query" "quandary" "question" "file" "path" "error"
-    "exception" "warning" "violation" "activity" "task" "performance" "branch"
-    "case" "switch" "function" "default" "handler" "clause" "handle" "queue"
-    "collection" "iterator" "item" "data" "transaction" "transformation"
-    "meme" "deity" "invocation" "matrix" "array" "set" "map" "hash" "test"
-    "condition" "consequent" "alternative" "scene" "graph" "plot" "wizard"
-    "anomaly" "prophecy" "operator" "functor" "application" "thread" "process"
-    "overflow" "jail" "escape" "recursion" "depth" "vector" "window"
-    "component" "operand" "machinery" "pipe" "widget" "spell" "arrow"
-    "database" "package" "statement" "expression" "justification" "parse"
-    "tree" "parser" "analyzer" "loader" "linker" "library" "module" "routine"
-    "class" "hierarchy" "attribute" "method" "callback" "monad" "architecture"
-    "target" "assembler" "language" "line" "column" "row" "scope" "closure"
-    "binding" "abstraction" "entry" "token" "trace" "hack" "dependency"
-    "resolution" "keyword" "stopword" "atom" "procedure" "directory" "mode"
-    "user" "flag" "box" "property" "phrase" "traceback" "stack" "heap"
-    "state" "arity" "leaf" "signal" "password" "character" "factory" "filter"
-    "lambda" "recipe" "transducer" "system" "interface" "implementation"
-    "logic" "form" "syntax" "icon" "effigy" "comment" "document" "namespace"
-    "primitive" "lexer" "reader" "loop" "event" "predicate" "promise"])
+  (rand-nth nouns))
 
 (defn subordinate-clause []
   [:choose
@@ -48,13 +77,32 @@
     [:span "at" noun]
     :nil :nil :nil])
 
+(defn upcase [s]
+  (if (empty? s)
+    s
+    (str (str/upper-case (str (first s)))
+         (str/join (rest s)))))
+
+(defn camel-case-exception []
+  (str/join
+    (map upcase
+         [(if (> (rand) 0.5)
+            (if (> (rand) 0.5) (badjective) "")
+            (weirdjective))
+          (rand-nth nouns)
+          (rand-nth ["error" "exception"])
+          ":"])))
+
+(defn midfix []
+  [:choose camel-case-exception :nil :nil])
+
 (defn suffix []
   [:choose
     (str "(" (rand-int 500) ":" (rand-int 100) ")")
     :nil :nil])
 
 (defn error []
-  [:span prefix badjective noun subordinate-clause suffix])
+  [:span prefix midfix badjective' noun subordinate-clause suffix])
 
 (defn emit [doc]
   (cond
